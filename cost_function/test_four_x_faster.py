2f56b8b37d3327b38bc76200d3a47c73cac301de
James Hoctor <JEHoctor@protonmail.com>
2019-11-29 10:45:33 -0500

    WIP: Bring in 4x faster cost function for testing and benchmarking.

---
+++ cost_function/test_four_x_faster.py
@@ -0,0 +1,31 @@
+from .four_x_faster import cost_function as cost_function_under_test
+from ..reference_cost_function import reference_cost_function
+from .. import dataset
+
+
+def both_cost_functions(prediction):
+    a = reference_cost_function(prediction)
+    b = cost_function_under_test(prediction)
+    assert a == b
+    return a
+
+def test_four_x_faster_cost_function():
+    submission = pd.read_csv(dataset.sample_submission_file, index_col='family_id')
+
+    best = submission['assigned_day'].tolist()
+    start_score = both_cost_functions(best)
+    
+    new = best.copy()
+    # loop over each family
+    for fam_id, _ in enumerate(best):
+        # loop over each family choice
+        for pick in range(10):
+            day = choice_dict[f'choice_{pick}'][fam_id]
+            temp = new.copy()
+            temp[fam_id] = day # add in the new pick
+            if both_cost_functions(temp) < start_score:
+                new = temp.copy()
+                start_score = both_cost_functions(new)
+    
+    score = both_cost_functions(new)
+    assert score == 672254.0276683343
