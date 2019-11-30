a7edf17ca3c13912d244653b110ca1efb265be6d
Alina Barnett <abarnett@cs.duke.edu>
2019-11-29 21:44:58 -0500

    import refine, matches sample score

--- latest_notebook.py
+++ latest_notebook.py
@@ -4,6 +4,7 @@ from santaspkg.constants import *
 from santaspkg.cost_function import cost_function
 from santaspkg.cost_function import soft_cost_function as cost_function
 from santaspkg.dataset import data, sample_submission
+from santaspkg.refinement import refine_until_convergence, refinement_pass
 
 #Make objects to reference later
 
@@ -17,19 +18,9 @@ days = list(range(N_DAYS,0,-1))
 
 # Start with the sample submission values
 best = sample_submission['assigned_day'].tolist()
-start_score = cost_function(best)
 
-new = best.copy()
-# loop over each family
-for fam_id, _ in enumerate(best):
-    # loop over each family choice
-    for pick in range(10):
-        day = choice_dict[f'choice_{pick}'][fam_id]
-        temp = new.copy()
-        temp[fam_id] = day # add in the new pick
-        if cost_function(temp) < start_score:
-            new = temp.copy()
-            start_score = cost_function(new)
+# Refine the sample submission
+new = refinement_pass(best)
 
 sample_submission['assigned_day'] = new
 score = cost_function(new)
