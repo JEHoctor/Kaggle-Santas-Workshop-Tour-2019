49e2e6480ee57f0f5aa2d715b9f586a8b5d71f07
James Hoctor <JEHoctor@protonmail.com>
2019-11-29 21:07:33 -0500

    Open data in dataset.py using pandas.

--- santaspkg/dataset.py
+++ santaspkg/dataset.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pandas as pd
 
 data_dir = Path(__file__).parent.parent / 'santa-workshop-tour-2019'
 family_data_file = data_dir / 'family_data.csv'
@@ -7,3 +8,6 @@ sample_submission_file = data_dir / 'sample_submission.csv'
 assert family_data_file.is_file() and sample_submission_file.is_file(), \
     ('The dataset must be downloaded from https://www.kaggle.com/c/santa-workshop-tour-2019/data'
      ' and extracted into '+str(data_dir))
+
+data = pd.read_csv(family_data_file, index_col='family_id')
+sample_submission = pd.read_csv(sample_submission_file, index_col='family_id')
