aaa33eb66096295eb592c53cf0720d9c76a766b2
James Hoctor <JEHoctor@protonmail.com>
2019-12-01 12:19:06 -0500

    Put submissions in a special directory to keep the root clean.

--- santaspkg/mk_submit.py
+++ santaspkg/mk_submit.py
@@ -1,13 +1,18 @@
 from santaspkg.cost_function import reference_cost_function
 from santaspkg.dataset import sample_submission
-import pandas as pd
+
+from pathlib import Path
 
 
 submission_template = sample_submission.copy(deep=True)
 
 
+submissions_dir = Path(__file__).parent.parent / 'submissions'
+submissions_dir.mkdir(exist_ok=True)
+
+
 def mk_submit(assignment):
     score = reference_cost_function(assignment)
-    filename = f'submission_{score}.csv'
+    filename = str(submissions_dir / f'submission_{score}.csv')
     submission_template['assigned_day'] = assignment
     submission_template.to_csv(filename)
