e937bb6a9c2b4c8d21b02a1162edfb373f409a89
James Hoctor <JEHoctor@protonmail.com>
2019-12-06 13:37:19 -0500

    These should be imported from cost_function.tables instead of from cost_function.

--- santaspkg/cost_function/__init__.py
+++ santaspkg/cost_function/__init__.py
@@ -1,4 +1,3 @@
 from santaspkg.cost_function.reference import reference_cost_function
 from santaspkg.cost_function.crescenzi import cost_function
 from santaspkg.cost_function.crescenzi import soft_cost_function
-from santaspkg.cost_function.tables import penalties, choice_array, penalty_matrix, accounting_matrix
